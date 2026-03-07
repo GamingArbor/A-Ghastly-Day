d0bddc1d653e69f27bfcc65ec319e6c093643e46
Wii-Bowler <nicosorensen4@gmail.com>
2026-03-07 16:41:19 -0500

    Refined Floating Mechanic + Added Floatover State

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
-
+var DirectionLock = 0
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # State Machine
 # Sets state to IDLE upon entering game
-enum States {IDLE, FLOAT, POSSESS, AIR, DEAD}
+enum States {IDLE, FLOAT, POSSESS, AIR, DEAD, FLOATOVER}
 var state = States.IDLE
 
 # Function for changing the player state as per the state machine
@@ -26,6 +26,9 @@ func _physics_process(delta: float) -> void:
 			air()
 		States.DEAD:
 			dead()
+		States.FLOATOVER:
+			floatover()
+		
 	
 	# The Evil Gravity
 	if not is_on_floor():
@@ -35,16 +38,19 @@ func _physics_process(delta: float) -> void:
 
 	# Floating Mechanic (Evil): Press float button to start floating, then press direction to go that way.
 	# This part is for setting the floating state
-	if Input.is_action_just_pressed("Float"):
+	if Input.is_action_just_pressed("Float") and is_on_floor():
 		set_state(States.FLOAT)
+		velocity.y = 0
+		DirectionLock = 0
 		print("Fish")
 
 	# The Movement (Basic Evil)
 	var direction := Input.get_axis("Left", "Right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if state != States.FLOAT and state != States.FLOATOVER:
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	move_and_slide()
 
@@ -53,17 +59,44 @@ func idle():
 
 func floating():
 	if Input.is_action_just_released("Float"):
-		set_state(States.IDLE)
+		set_state(States.FLOATOVER)
 		print("Bass")
+		
+	# Locks Direction into vertical or horizontal; 1 = vertical, 2 = horizontal
 	
+	if DirectionLock == 0:
+		if Input.is_action_pressed("Up") or Input.is_action_pressed("Down"):
+			DirectionLock = 1
+	if DirectionLock == 0:
+		if Input.is_action_pressed("Left") or Input.is_action_pressed("Right"):
+			DirectionLock = 2
+	
+	var direction = 0
+	
+	if DirectionLock == 1:
+		if Input.is_action_pressed("Up"):
+			velocity.y = -SPEED
+		elif Input.is_action_pressed("Down"):
+			velocity.y = SPEED
+		elif Input.is_action_just_released("Up") or Input.is_action_just_released("Down"):
+			velocity.y = 0
+	elif DirectionLock == 2:
+		if Input.is_action_pressed("Left"):
+			direction = -1
+		elif Input.is_action_pressed("Right"):
+			direction = 1
+		elif Input.is_action_just_released("Left") or Input.is_action_just_released("Right"):
+			direction = 0
 	# Handle Floating Up and Down
-	if Input.is_action_pressed("Up"):
-		velocity.y = -SPEED
-	elif Input.is_action_pressed("Down"):
-		velocity.y = SPEED
-	elif Input.is_action_just_released("Up") or Input.is_action_just_released("Down"):
-		velocity.y = 0
 
+	
+
+
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 func possess():
 	pass
 
@@ -73,6 +106,9 @@ func air():
 func dead():
 	pass
 
+func floatover():
+	if is_on_floor():
+		set_state(States.IDLE)
 # When you want to change the player's current state, use this:
 # set_state(States.IDLE)
 # States.IDLE could also be a different state such as FLOAT or DEAD
