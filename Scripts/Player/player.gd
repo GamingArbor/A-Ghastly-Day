784b0e35a67b66c452252104d28c4beecf1c9c10
Wii-Bowler <nicosorensen4@gmail.com>
2026-03-06 23:21:46 -0500

    Added Semi-Solids + Player Inputs

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -6,16 +6,15 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# The Evil Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Floating Mechanic (Evil): Press float button to start floating, then press direction to go that way.
+	if Input.is_action_just_pressed("Float"):
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# The Movement (Basic Evil)
 	var direction := Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
