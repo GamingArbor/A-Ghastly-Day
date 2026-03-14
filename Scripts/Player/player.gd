85f2006237e7da5b75d1e6330b9380287316f712
AnImpossibility <105756699+AnImpossibility@users.noreply.github.com>
2026-03-14 18:48:26 -0400

    Cleaned up player.gd
    Self-explanatory

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+# Movement Constants
 const SPEED = 150.0
 const DRAGSPEED = SPEED * 0.75
 const JUMP_VELOCITY = -400.0
@@ -9,19 +10,15 @@ const JUMP_VELOCITY = -400.0
 var States = Global.States
 var state = States.IDLE
 
-# For locking the axis the player floats in
-var DirectionLock = 0
+var DirectionLock = 0 # For locking the axis the player floats in
+var InDeadBodyRange: bool = false # For determining when the player can interact with the 
 
-# For checking to see if the player can intact with various objects
-var InDeadBodyRange: bool = false
-
-
-# Function for changing the player state as per the state machine
+# Utility function for changing the player state as per the state machine
 func set_state(newState):
 	state = newState
 
 func _physics_process(delta: float) -> void:
-	#Run functions associated with states
+	## Run functions associated with states
 	match state:
 		States.IDLE:
 			idle()
@@ -38,24 +35,26 @@ func _physics_process(delta: float) -> void:
 		States.DRAG:
 			drag()
 		
-	# Update the Global Player_State
-	Global.PlayerState = state
+	## Gravity (does not apply when floating)
+	if not is_on_floor() and state != States.FLOAT:
+		velocity += get_gravity() * delta
 	
-	# The Evil Gravity
-	if not is_on_floor():
-		# Check for Floating State to prevent gravity from applying while the player is floating
-		if state != States.FLOAT:
-			velocity += get_gravity() * delta
+	## Grounded movement (Basic Evil)
+	var direction := Input.get_axis("Left", "Right")
+	if state != States.FLOAT and state != States.FLOATOVER:
+		if direction:
+			# Player speed is reduced when dragging
+			velocity.x = direction * (DRAGSPEED if state == States.DRAG else SPEED)
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	# Floating Mechanic (Evil): Press float button to start floating, then press direction to go that way.
-	# This part is for setting the floating state
-	# Checks to make sure the player is on the floor and not dragging the body
-	if Input.is_action_just_pressed("Float") and is_on_floor() and state != States.DRAG:
-		set_state(States.FLOAT)
-		velocity.y = 0
-		DirectionLock = 0
+	# Floating Mechanic (Evil): Applies when grounded, not dragging, and pressing the float button
+	if is_on_floor() and state != States.DRAG and Input.is_action_pressed("Float") and state != States.FLOAT:
+			set_state(States.FLOAT) # Set the state to floating (notably prevents rerunning)
+			velocity.y = 0
+			DirectionLock = 0
 	
-	# Handles moving down semi-solid platforms
+	# Moving down semi-solid platforms
 	if Input.is_action_pressed("Down"):
 		set_collision_layer_value(2, false)
 		set_collision_mask_value(2, false)
@@ -63,25 +62,8 @@ func _physics_process(delta: float) -> void:
 		set_collision_layer_value(2, true)
 		set_collision_mask_value(2, true)
 	
-	# The Movement (Basic Evil)
-	var direction := Input.get_axis("Left", "Right")
-	if state != States.FLOAT and state != States.FLOATOVER:
-		if direction:
-			# Reduces speed of player if they are dargging the body
-			if state == States.DRAG:
-				velocity.x = direction * DRAGSPEED
-			else:
-				velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	move_and_slide()
-	
-	# Initiates the Dragging Body state if the player is in range of the body
-	if InDeadBodyRange == true and Input.is_action_just_pressed("Interact"):
-		%Deadbody.reparent(self)
-		snap_to_body()
-		set_state(States.DRAG)
 
 
 # Allows for player script to know if it can pick up the body
@@ -95,24 +77,26 @@ func snap_to_body():
 
 
 func idle():
-	pass
+	$AnimationPlayer.play("Idle")
+	# Initiates the Dragging Body state if the player is in range of the body
+	if InDeadBodyRange == true and Input.is_action_just_pressed("Interact"):
+		%Deadbody.reparent(self)
+		snap_to_body()
+		set_state(States.DRAG)
 
 func floating():
 	if Input.is_action_just_released("Float"):
 		set_state(States.FLOATOVER)
 	
 	# Locks Direction into vertical or horizontal; 1 = vertical, 2 = horizontal
-	
 	if DirectionLock == 0:
 		if Input.is_action_pressed("Up") or Input.is_action_pressed("Down"):
 			DirectionLock = 1
-	if DirectionLock == 0:
-		if Input.is_action_pressed("Left") or Input.is_action_pressed("Right"):
+		elif Input.is_action_pressed("Left") or Input.is_action_pressed("Right"):
 			DirectionLock = 2
-	
 	var direction = 0
 	
-	# Handle moving in orthagonal directions while floating
+	# Handle moving in orthogonal directions while floating
 	if DirectionLock == 1:
 		if Input.is_action_pressed("Up"):
 			velocity.y = -SPEED
@@ -151,6 +135,7 @@ func floatover():
 		set_state(States.IDLE)
 
 func drag():
+	$AnimationPlayer.play("Dragging")
 	$DragJoint.node_a = self.get_path()
 	$DragJoint.node_b = %Deadbody.get_path()
 	if Input.is_action_just_released("Interact"):
@@ -159,7 +144,7 @@ func drag():
 		$DragJoint.node_a = NodePath("")
 		$DragJoint.node_b = NodePath("")
 		
-
+## Development Notes
 # When you want to change the player's current state, use this:
 # set_state(States.IDLE)
 # States.IDLE could also be a different state such as FLOAT or DEAD
