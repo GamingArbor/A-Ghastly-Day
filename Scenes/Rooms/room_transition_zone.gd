84ef4d3aa3bc81d681012cbcb4a8034796354738
AnImpossibility <105756699+AnImpossibility@users.noreply.github.com>
2026-03-14 21:56:33 -0400

    Really Tiny Change Seriously Its Small
    Removed default room number from room scene

--- Scenes/Rooms/room_transition_zone.gd
+++ Scenes/Rooms/room_transition_zone.gd
@@ -1,7 +1,7 @@
 extends Area2D
 var PreviousRoomNumber: int
 var PreviousCameraPosition: Vector2
-@export var RoomNumber = 0
+@export var RoomNumber: int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
