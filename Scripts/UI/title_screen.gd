317c14507c3599c19e1cd87569963aaa8d1d6d30
GamingArbor <liamfflanagan@gmail.com>
2026-03-25 17:48:31 -0400

    Added Info Menu
    The menu exists to communicate controls to the player

--- Scripts/UI/title_screen.gd
+++ Scripts/UI/title_screen.gd
@@ -3,8 +3,10 @@ extends Node2D
 # Used for denying input while in the title sequence
 var DenyPlayerInput : bool = true
 # Tracks the player's position in the menu (Which button they are currently selecting/on)
-# 0=Start 1=Options 2=Exit
+# 0=Start 1=Info 2=Exit
 var MenuPosition : int = 0
+# Tracks whether the player is in the info menu or not
+var InInfoMenu : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -34,7 +36,7 @@ func _process(delta: float) -> void:
 	
 	# Handles pressing buttons
 	# Checks to see if input is accepted
-	if Input.is_action_just_pressed("Float") and DenyPlayerInput == false:
+	if Input.is_action_just_pressed("Float") and DenyPlayerInput == false and InInfoMenu == false:
 		match MenuPosition:
 			# Functionality for Start Button
 			0:
@@ -44,11 +46,20 @@ func _process(delta: float) -> void:
 			# Functionality for Options Button
 			1:
 				$Buttons/OptionsButton.texture_normal = preload("res://Graphics/UI/TitleMenu-Button-Info-Pressed.png")
+				$AnimationPlayer.play("InfoMenuEnter")
+				DenyPlayerInput = true
+				InInfoMenu = true
 			# Functionality for Exit Button
 			2:
 				$Buttons/ExitButton.texture_normal = preload("res://Graphics/UI/TitleMenu-Button-Exit-Pressed.png")
 				$AnimationPlayer.play("ExitGame")
 				DenyPlayerInput = true
+	
+	# Handles leaving the info menu
+	if InInfoMenu == true and DenyPlayerInput == false and Input.is_action_just_pressed("Interact"):
+		$AnimationPlayer.play("InfoMenuExit")
+		DenyPlayerInput = true
+		InInfoMenu = false
 	
 	# Handles movement in the menu
 	if Input.is_action_just_pressed("Up") and MenuPosition > 0:
@@ -58,7 +69,7 @@ func _process(delta: float) -> void:
 		MenuPosition += 1
 	
 
-# Called when: Title sequence ends
+# Called when: Title sequence ends, entering or exiting info menu
 func AllowInput() -> void:
 	DenyPlayerInput = false
 
