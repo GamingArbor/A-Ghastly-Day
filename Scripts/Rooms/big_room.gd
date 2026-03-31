42be7a94705acb6fbce99c2b14fb24e772050892
GamingArbor <liamfflanagan@gmail.com>
2026-03-31 00:08:48 -0400

    Fixed Some Level Design

--- Scripts/Rooms/big_room.gd
+++ Scripts/Rooms/big_room.gd
@@ -82,3 +82,8 @@ func ResumeGame() -> void:
 func ExitToTitle() -> void:
 	DenyInputInMenu = false
 	get_tree().change_scene_to_file("res://Scenes/UI/title_screen.tscn")
+
+
+func _on_end_game_detection_body_entered(body: Node2D) -> void:
+	if body == %Player:
+		
