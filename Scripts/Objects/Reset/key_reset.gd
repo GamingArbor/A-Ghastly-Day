a1a52d7ea6aca41511151ac5acdbc6e7dacabdd1
AnImpossibility <105756699+AnImpossibility@users.noreply.github.com>
2026-04-01 00:16:29 -0400

    Finished Door & Key Resetting

--- Scripts/Objects/Reset/key_reset.gd
+++ Scripts/Objects/Reset/key_reset.gd
@@ -1,9 +1,13 @@
 extends Component
 
+@onready var Transform: Transform2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.restart.connect(reset)
+	Transform = get_parent_object().transform
 
 func reset():
+	get_parent_object().set_transform(Transform)
 	get_parent_object().find_child("CollisionShape2D").disabled = false
 	get_parent_object().find_child("Sprite2D").visible = true
